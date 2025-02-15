cc0e8b29170e353153b1c40b29d565822fc0d046
acramin <132026903+acramin@users.noreply.github.com>
2025-02-14 22:22:17 -0300

    aula01
    css + react
    + arquivos base

---
+++ Aula01/Gabarito Aula 1/src/components/Main.jsx
@@ -0,0 +1,514 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="item-list">
+        <div className="item-list__header">
+          <h2>Artistas Populares</h2>
+          <a href="/">Mostrar Tudo</a>
+        </div>
+
+        <div className="item-list__container">
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__div-image "
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Henrique & Juliano"
+                />
+              </div>
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Medley Canoa - Putaria no Escuro / Menina do Job / Escada do
+                  Prédio / Mal Caminho / Pede Que eu Te Dou
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__div-image "
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Henrique & Juliano"
+                />
+              </div>
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Medley Canoa - Putaria no Escuro / Menina do Job / Escada do
+                  Prédio / Mal Caminho / Pede Que eu Te Dou
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__div-image "
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Henrique & Juliano"
+                />
+              </div>
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Medley Canoa - Putaria no Escuro / Menina do Job / Escada do
+                  Prédio / Mal Caminho / Pede Que eu Te Dou
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__div-image "
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Henrique & Juliano"
+                />
+              </div>
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Medley Canoa - Putaria no Escuro / Menina do Job / Escada do
+                  Prédio / Mal Caminho / Pede Que eu Te Dou
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__div-image "
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Henrique & Juliano"
+                />
+              </div>
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Medley Canoa - Putaria no Escuro / Menina do Job / Escada do
+                  Prédio / Mal Caminho / Pede Que eu Te Dou
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__div-image "
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Henrique & Juliano"
+                />
+              </div>
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Medley Canoa - Putaria no Escuro / Menina do Job / Escada do
+                  Prédio / Mal Caminho / Pede Que eu Te Dou
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="item-list">
+        <div className="item-list__header">
+          <h2>Músicas Populares</h2>
+          <a href="/">Mostrar Tudo</a>
+        </div>
+
+        <div className="item-list__container">
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__div-image "
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Henrique & Juliano"
+                />
+              </div>
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Medley Canoa - Putaria no Escuro / Menina do Job / Escada do
+                  Prédio / Mal Caminho / Pede Que eu Te Dou
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__div-image "
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Henrique & Juliano"
+                />
+              </div>
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Medley Canoa - Putaria no Escuro / Menina do Job / Escada do
+                  Prédio / Mal Caminho / Pede Que eu Te Dou
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__div-image "
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Henrique & Juliano"
+                />
+              </div>
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Medley Canoa - Putaria no Escuro / Menina do Job / Escada do
+                  Prédio / Mal Caminho / Pede Que eu Te Dou
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__div-image "
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Henrique & Juliano"
+                />
+              </div>
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Medley Canoa - Putaria no Escuro / Menina do Job / Escada do
+                  Prédio / Mal Caminho / Pede Que eu Te Dou
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__div-image "
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Henrique & Juliano"
+                />
+              </div>
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Medley Canoa - Putaria no Escuro / Menina do Job / Escada do
+                  Prédio / Mal Caminho / Pede Que eu Te Dou
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__div-image "
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Henrique & Juliano"
+                />
+              </div>
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Medley Canoa - Putaria no Escuro / Menina do Job / Escada do
+                  Prédio / Mal Caminho / Pede Que eu Te Dou
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__div-image "
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Henrique & Juliano"
+                />
+              </div>
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Medley Canoa - Putaria no Escuro / Menina do Job / Escada do
+                  Prédio / Mal Caminho / Pede Que eu Te Dou
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__div-image "
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Henrique & Juliano"
+                />
+              </div>
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Medley Canoa - Putaria no Escuro / Menina do Job / Escada do
+                  Prédio / Mal Caminho / Pede Que eu Te Dou
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__div-image "
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Henrique & Juliano"
+                />
+              </div>
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Medley Canoa - Putaria no Escuro / Menina do Job / Escada do
+                  Prédio / Mal Caminho / Pede Que eu Te Dou
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__div-image "
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Henrique & Juliano"
+                />
+              </div>
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Medley Canoa - Putaria no Escuro / Menina do Job / Escada do
+                  Prédio / Mal Caminho / Pede Que eu Te Dou
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__div-image "
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Henrique & Juliano"
+                />
+              </div>
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Medley Canoa - Putaria no Escuro / Menina do Job / Escada do
+                  Prédio / Mal Caminho / Pede Que eu Te Dou
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__div-image "
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Henrique & Juliano"
+                />
+              </div>
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Medley Canoa - Putaria no Escuro / Menina do Job / Escada do
+                  Prédio / Mal Caminho / Pede Que eu Te Dou
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
