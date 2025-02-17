2fd11b2ac6a8ab1456a53841009e96fff158df05
acramin <132026903+acramin@users.noreply.github.com>
2025-02-17 00:09:39 -0300

    mais ajustes

--- front/api/api.js
+++ front/api/api.js
@@ -1,8 +1,6 @@
-import "dotenv/config";
 import axios from "axios";
 
-//const NODE_ENV = process.env.NODE_ENV;
-//const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+//const URL = "http://localhost:3000/api" ;
 const URL = "https://spotify-clone-9dey.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
